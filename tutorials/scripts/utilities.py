4c2b95a535b7ccf432c0f9aa5994eaac58751a3f
mstechly <michal.stechly@gmail.com>
2018-08-31 08:20:10 +0200

    03 Naive TSP - first try + cost operators.

--- tutorials/scripts/utilities.py
+++ tutorials/scripts/utilities.py
@@ -1,6 +1,16 @@
 import numpy as np
 import itertools
 
+def create_cities(N):
+    """
+    Creates an array of random points of size N.
+    """
+    cities = []
+    for i in range(N):
+        cities.append(np.random.rand(2) * 10)
+    return np.array(cities)
+    
+
 def distance_between_points(point_A, point_B):
     return np.sqrt((point_A[0] - point_B[0])**2 + (point_A[1] - point_B[1])**2)
 
@@ -48,3 +58,28 @@ def solve_tsp_brute_force(cities, starting_city, verbose=True):
         print("Best route:", best_permutation)
         print("Cost:", best_cost)
     return best_permutation
+
+def points_order_to_binary_state(points_order):
+    """
+    Transforms the order of points from the standard representation: [0, 1, 2],
+    to the binary one: [1,0,0,0,1,0,0,0,1]
+    """
+    number_of_points = len(points_order)
+    binary_state = np.zeros((len(points_order))**2)
+    for j in range(len(points_order)):
+        p = points_order[j]
+        binary_state[(number_of_points) * (j) + (p)] = 1
+    return binary_state
+
+def binary_state_to_points_order(binary_state):
+    """
+    Transforms the the order of points from the binary representation: [1,0,0,0,1,0,0,0,1],
+    to the binary one: [0, 1, 2]
+    """
+    points_order = []
+    number_of_points = int(np.sqrt(len(binary_state)))
+    for p in range(number_of_points):
+        for j in range(number_of_points):
+            if binary_state[(number_of_points) * p + j] == 1:
+                points_order.append(j)
+    return points_order
